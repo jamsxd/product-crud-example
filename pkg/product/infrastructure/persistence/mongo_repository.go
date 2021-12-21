b7a32ee88e5aed7e491c0ae5bdf56fec4abf084e
Johans Morales <jams.93.jm@gmail.com>
2021-12-21 13:58:51 +0100

    mod findAll and findBySky method

--- pkg/product/infrastructure/persistence/mongo_repository.go
+++ pkg/product/infrastructure/persistence/mongo_repository.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/jamsxd/product-crud-example/pkg/product/domain"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -14,18 +15,18 @@ type basicMongoRepository struct {
 }
 
 func NewMongoRepository(client *mongo.Client, dbName, collection string) domain.ProductRepository {
-	return &basicMongoRepository{collection: client.Database(dbName).Collection("products")}
+	return &basicMongoRepository{collection: client.Database(dbName).Collection(collection)}
 }
 
-func (r *basicMongoRepository) FindAll(ctx context.Context) ([]*domain.Product, error) {
+func (r *basicMongoRepository) FindAll(ctx context.Context) ([]domain.Product, error) {
 	ctxTimeout, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
-	cursor, err := r.collection.Find(ctxTimeout, nil, options.Find())
+	cursor, err := r.collection.Find(ctxTimeout, bson.D{})
 	if err != nil {
 		return nil, err
 	}
 
-	var products []*domain.Product
+	var products []domain.Product
 	if err := cursor.All(ctx, &products); err != nil {
 		return nil, err
 	}
@@ -37,7 +38,13 @@ func (r *basicMongoRepository) FindBySku(ctx context.Context, sku string) (*doma
 	var product domain.Product
 	ctxTimeout, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
-	err := r.collection.FindOne(ctxTimeout, map[string]interface{}{"sku": sku}).Decode(&product)
+	err := r.collection.FindOne(ctxTimeout, bson.D{{
+		Key:   "sku",
+		Value: sku,
+	}}).Decode(&product)
+	if err != nil && err == mongo.ErrNoDocuments {
+		return nil, domain.ErrProductNotFound
+	}
 	return &product, err
 }
 
